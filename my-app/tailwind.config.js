0831fc33f581c10b85c89b6a99d9a47175d09e2e
smilling <nikita.niki2410@gmail.com>
2025-06-05 15:57:12 +0700

    file

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2B4763',
+        secondary: {
+          DEFAULT: '#FBF9E7',
+          green: '#588980',
+          yellow: '#EAE6A1',
+          lightgreen: '#CCD885'
+        },
+        black: '#2E2E2E',
+        gray: '#9B9B9B',
+      },
+      fontFamily: {
+        aExLight: ["Assistant-ExtraLight", "sans-serif"],
+        aLight: ["Assistant-Light", "sans-serif"],
+        aRegular: ["Assistant-Regular", "sans-serif"],
+        aMedium: ["Assistant-Medium", "sans-serif"],
+        aSemiBold: ["Assistant-SemiBold", "sans-serif"],
+        aBold: ["Assistant-Bold", "sans-serif"],
+        aExBold: ["Assistant-ExtraBold", "sans-serif"],
+        yThin: ["Yantramanav-Thin", "sans-serif"],
+        yRegular: ["Yantramanav-Regular", "sans-serif"],
+        yMedium: ["Yantramanav-Medium", "sans-serif"],
+        yLight: ["Yantramanav-Light", "sans-serif"],
+        yBold: ["Yantramanav-Bold", "sans-serif"],
+        yBlack: ["Yantramanav-Black", "sans-serif"],
+        rMedium: ["Raleway-Medium", "yantramanav", "sans-serif"],
+      },
+
+      fontSize: {
+        'xxs': '0.625rem'
+      }
+    },
+  },
+  plugins: [],
+}
