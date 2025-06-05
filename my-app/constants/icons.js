306012861ae8991919dc04c180283f67f6738ea4
smilling <nikita.niki2410@gmail.com>
2025-06-05 15:59:29 +0700

    file

---
+++ my-app/constants/icons.js
@@ -0,0 +1,13 @@
+import home from "../assets/icons/home.png";
+import volunteer from "../assets/icons/volunteer.png";
+import donate from "../assets/icons/donate.png";
+import donor from "../assets/icons/donor.png";
+import track from "../assets/icons/track.png";
+
+export default {
+    home,
+    volunteer,
+    donate,
+    donor,
+    track,
+}
---
+++ my-app/constants/images.js
@@ -0,0 +1,13 @@
+import logo from "../assets/images/logo.png";
+import apple from "../assets/images/apple.png";
+import google from "../assets/images/google.png";
+import facebook from "../assets/images/facebook.png";
+import twitter from "../assets/images/twitter.png";
+
+export default {
+    logo,
+    apple,
+    google,
+    facebook,
+    twitter,
+}
